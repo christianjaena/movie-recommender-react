478f7997699227a09198bbd4aa2e4fd03555a68c
Christian Jaena <christian.jaena@msugensan.edu.ph>
2020-07-27 20:15:04 +0800

    changed UI and integrated yts download links

--- src/App.js
+++ src/App.js
@@ -13,15 +13,24 @@ const App = () => {
 			const response = await fetchUrl.json();
 			const movieArray = await Promise.all(
 				response.results.map(
-					async ({ id, title, overview, release_date, poster_path }) => {
+					async ({
+						id,
+						title,
+						overview,
+						release_date,
+						poster_path,
+						backdrop_path,
+					}) => {
 						const trailer = await getTrailer(id);
-						const poster = `https://image.tmdb.org/t/p/w300/${poster_path}`;
+						const poster = `https://image.tmdb.org/t/p/original/${poster_path}`;
+						const backdrop = `https://image.tmdb.org/t/p/original/${backdrop_path}`;
 						return {
 							id,
 							title,
 							overview,
 							release_date,
 							poster,
+							backdrop,
 							trailer,
 						};
 					}
@@ -42,27 +51,89 @@ const App = () => {
 		const youtubeKey = filteredArray[0]?.key;
 		return youtubeKey ? `https://www.youtube.com/embed/${youtubeKey}` : '';
 	};
+
 	return (
-		<div className='App'>
+		<div
+			style={{
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'center',
+				flexDirection: 'column',
+			}}
+		>
 			{isLoaded
 				? movies.map(
-						({ id, title, overview, poster, release_date, trailer }) => {
+						({
+							id,
+							title,
+							overview,
+							poster,
+							release_date,
+							trailer,
+							backdrop,
+						}) => {
 							return (
-								<div key={id}>
-									<img src={poster} alt='poster' />
-									<h1>{title}</h1>
-									<h3>{release_date}</h3>
-									<p>{overview}</p>
-									{trailer ? (
-										<iframe
-											title='trailer'
-											width='420'
-											height='315'
-											src={trailer}
-										></iframe>
-									) : (
-										<h1>No trailer for this movie yet</h1>
-									)}
+								<div key={id} style={{ display: 'flex', height: '50%' }}>
+									<div
+										style={{ padding: '20px', height: '100%', width: '30%' }}
+									>
+										<img
+											src={poster}
+											alt='poster'
+											style={{
+												position: 'relative',
+												height: '100%',
+												width: '100%',
+											}}
+										/>
+									</div>
+									<div
+										style={{
+											padding: '0 25px 25px 25px',
+											width: '70%',
+											display: 'flex',
+											flexDirection: 'column',
+											justifyContent: 'space-between',
+										}}
+									>
+										<div style={{ height: 'auto' }}>
+											<p
+												style={{
+													fontWeight: 'bold',
+													fontSize: '2em',
+													marginTop: '10px',
+												}}
+											>
+												{title}
+											</p>
+											<span style={{height: '100px', width:'200px', backgroundColor:'green', borderRadius:'20px', padding: '15px'}}>
+												<a
+													href={`https://yts.mx/movies/${title
+														.replace(/\s+/g, '-')
+														.toLowerCase()}-${release_date.substring(0, 4)}`}
+														target='_blank'
+														rel='noopener noreferrer'
+													style={{textDecoration: 'none', color: 'white'}}
+												>
+													Download
+												</a>
+											</span>
+											<p>{release_date}</p>
+											<p>{overview}</p>
+										</div>
+										<div style={{ height: '70%' }}>
+											{trailer ? (
+												<iframe
+													title='trailer'
+													width='100%'
+													height='100%'
+													src={trailer}
+												></iframe>
+											) : (
+												<h1>No trailer for this movie yet</h1>
+											)}
+										</div>
+									</div>
 								</div>
 							);
 						}
