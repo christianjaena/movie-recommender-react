dea3b95688908da531c6a34f1b9723a7faafe698
Christian Jaena <christian.jaena@msugensan.edu.ph>
2020-07-31 12:55:12 +0800

    Updates

---
+++ precache-manifest.d2c58c593ab2ca0a1fb631691bf368e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24ee449d8f7a2bb058e3f647e594d8a9",
+    "url": "/weekly-movie-trends/index.html"
+  },
+  {
+    "revision": "5d20ba14a7c9ada6a802",
+    "url": "/weekly-movie-trends/static/css/main.4034680b.chunk.css"
+  },
+  {
+    "revision": "551c5a6f9bcb1f991cec",
+    "url": "/weekly-movie-trends/static/js/2.8962840a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weekly-movie-trends/static/js/2.8962840a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d20ba14a7c9ada6a802",
+    "url": "/weekly-movie-trends/static/js/main.980bc971.chunk.js"
+  },
+  {
+    "revision": "1428d4d6df65a0ab8027",
+    "url": "/weekly-movie-trends/static/js/runtime-main.a4482f5e.js"
+  }
+]);
